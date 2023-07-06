3df4be07e0c0e19a27a5f3a807d8cbf0cefc5397
juan <juanmartin42453@gmail.com>
2023-07-06 05:02:45 -0300

    minimum viable product completed

--- index.js
+++ index.js
@@ -7,4 +7,20 @@ function switchClass(){
 }
 
 
+const hideButton = document.getElementById("hideMenu");
+const menuItem = document.querySelectorAll(".menu__item");
+const header = document.querySelector(".menu__container");
+function Hide(){
 
+    header.classList.toggle("hidden")
+    const applyClass = () => {
+        for (i in menuItem){
+            menuItem[i].classList.toggle("hidden");
+        }
+        ;
+    }
+    applyClass()    
+    console.log(menuItem)
+}
+
+hideButton.addEventListener("click", Hide);
