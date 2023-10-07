1408a2e4f439b84b702d159d6c8ae5e26917046e
JuanMartinPC <juanmartin42453@gmail.com>
2023-10-06 23:26:27 -0300

    responsive para celular, falta tablet.

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const button = document.getElementById("button");
 const menu = document.getElementById("menu");
 button.addEventListener("click", switchClass);
+const menuItem = document.querySelectorAll(".menu__item");
 
 function switchClass(){
     menu.classList.toggle("active");
+    const main = document.getElementById('main');
+    if (menu.classList.contains('active')){
+        main.addEventListener('click', () => {
+            menu.classList.remove('active')
+        })
+    }
+    else if (menu.classList.contains('active')){
+        menuItem.addEventListener('click', () => {
+            menu.classList.remove('active')
+        })
+    }
 }
 
-
 const hideButton = document.getElementById("hideMenu");
-const menuItem = document.querySelectorAll(".menu__item");
 const header = document.querySelector(".menu__container");
+
 function Hide(){
 
     header.classList.toggle("hidden")
@@ -17,23 +28,8 @@ function Hide(){
         for (i in menuItem){
             menuItem[i].classList.toggle("hidden");
         }
-        ;
     }
-    applyClass()    
-    console.log(menuItem)
+    applyClass()
 }
 
 hideButton.addEventListener("click", Hide);
-
-
-/* const openImage = document.getElementById("project__grid-container")
-
-function openPopUp(){
-    const getImages = () => {
-        for (i in openImage){
-            console.log(openImage[i])
-        };
-    }
-
-    openImage.addEventListener('click', getImages);;
-} */
